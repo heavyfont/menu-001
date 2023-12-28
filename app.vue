15753b5a2da64a146508c531ff22ea3a6cf13f09
vicky <vickywitvicky@gmail.com>
2023-12-28 10:36:18 +0800

    update font loader v4

--- app.vue
+++ app.vue
@@ -5,58 +5,16 @@ const { $gsap } = useNuxtApp()
   useHead({
 
   })
-  useFont([
-    {
-      src: '/fonts/LibreCaslonCondensed-Medium.woff2',
-      family: 'Libre Med',
-      weight: '600',
-      fallback: ['serif'],
-      variable: 'font_libre_med',
-      preload: true, 
-    },
-    {
-      src: '/fonts/LibreCaslonCondensed-Regular.woff2',
-      family: 'Libre',
-      weight: '400',
-      fallback: ['serif'],
-      variable: 'font_libre',
-      preload: true,
-    },
-    {
-      src: '/fonts/OverusedGrotesk-Bold.woff2',
-      family: 'Over Bold',
-      weight: '100 900',
-      fallback: ['sans-serif'],
-      preload: true, 
-    },
-    {
-      src: '/fonts/OverusedGrotesk-Medium.woff2',
-      family: 'Over Med',
-      weight: '100 900',
-      fallback: ['sans-serif'],
-      preload: true, 
-    },
-  ])
-  
-// Function to load fonts using the FontFace API
-async function loadFonts() {
-  const fontPromises = [
-    new FontFace('Libre Med', 'url(/fonts/LibreCaslonCondensed-Medium.woff2)', { weight: '600' }).load(),
-    new FontFace('Libre', 'url(/fonts/LibreCaslonCondensed-Regular.woff2)', { weight: '400' }).load(),
-    new FontFace('Over Bold', 'url(/fonts/OverusedGrotesk-Bold.woff2)', { weight: '800' }).load(),
-    new FontFace('Over Med', 'url(/fonts/OverusedGrotesk-Medium.woff2)', { weight: '600' }).load(),
-    // Add other font configurations
-  ];
 
-  try {
-    await Promise.all(fontPromises);
-  } catch (error) {
-    console.error('Error loading fonts:', error);
-  }
-}
 
-  onMounted(async () => {
-      await loadFonts(); // Wait for fonts to be loaded
+  onMounted(() => {
+  // nuxtApp.hook("page:start", () => {
+  //   isLoading.value = true;
+  // });
+
+  // nuxtApp.hook("page:finish", () => {
+  //   isLoading.value = false;
+  // });
   });
 </script>
 <template>
@@ -76,6 +34,5 @@ async function loadFonts() {
   margin: 0 auto;
   padding: 0.5rem;
 }
-
 
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,36 @@
 <script setup>
+  useFont([
+    {
+      src: '/fonts/LibreCaslonCondensed-Medium.woff2',
+      family: 'Libre Med',
+      weight: '600',
+      fallback: ['serif'],
+      variable: 'font_libre_med',
+      preload: true, 
+    },
+    {
+      src: '/fonts/LibreCaslonCondensed-Regular.woff2',
+      family: 'Libre',
+      weight: '300',
+      fallback: ['serif'],
+      variable: 'font_libre',
+      preload: true,
+    },
+    {
+      src: '/fonts/OverusedGrotesk-Bold.woff2',
+      family: 'Over Bold',
+      weight: '100 900',
+      fallback: ['sans-serif'],
+      preload: true, 
+    },
+    {
+      src: '/fonts/OverusedGrotesk-Medium.woff2',
+      family: 'Over Med',
+      weight: '100 900',
+      fallback: ['sans-serif'],
+      preload: true, 
+    },
+  ])
 </script>
 	<template>
 		<div class="layout">
